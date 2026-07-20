e3c0fe99d6c012706e7cf995e6f14b0984317d94
normaPeerez <nlmg.16.21@gmail.com>
2026-07-19 20:25:33 -0600

    feat: Implementar funcion para registrar autores

--- autores.py
+++ autores.py
@@ -2,3 +2,4 @@
 class Autor:
     def __init__(self, nombre, nacionalidad): self.nombre = nombre; self.nacionalidad = nacionalidad
 lista_autores = []
+def registrar_autor(autor): lista_autores.append(autor)
