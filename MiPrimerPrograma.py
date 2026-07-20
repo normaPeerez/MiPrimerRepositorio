6e54a94fdfaef0bb5d02f5e26fa9cbd805be4166
normaPeerez <nlmg.16.21@gmail.com>
2026-07-19 18:49:51 -0600

    Se mejoró la interfaz del programa.

--- MiPrimerPrograma.py
+++ MiPrimerPrograma.py
@@ -2,3 +2,4 @@ nombre = input("Ingrese su nombre: ")
 edad = int(input("Ingrese su edad: "))
 print("hola", nombre , " y usted tiene " , edad)
 # Se agregó validación de datos
+# Se mejoró la interfaz del programa
